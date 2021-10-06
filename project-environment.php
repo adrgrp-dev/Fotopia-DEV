4d3246938bf783e469fb6654d4475b8b8d073d4c
SidambaraSelvanADR <ssselvan.83@gmail.com>
2021-10-06 15:00:55 +0530

    1 Email - SMTP credentials kept in project environment files as common. 2. Updated subject and body message heading sentence in all the email code

--- project-environment.php
+++ project-environment.php
@@ -2,8 +2,21 @@
  $_SESSION['project_url']="http://fotopia.adrgrp.com/photo-dev/";
 $application_url="http://fotopia.adrgrp.com/photo-dev";
 
+
+//Database Credentials
 $dbhost = "localhost";
  $dbuser = "root";
  $dbpass = "";
  $db = "photography_app";
+ 
+// Email credentials
+$emailHost='smtp.gmail.com';
+$emailUserID='test.deve@adrgrp.com';
+$emailPassword='adrgrp@123';
+
+$_SESSION['emailHost']=$emailHost;
+$_SESSION['emailUserID']=$emailUserID;
+$_SESSION['emailPassword']=$emailPassword;
+
+
 ?>
