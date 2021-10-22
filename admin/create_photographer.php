27cd2d8f540dacfae35cf8d16a68d16522f095dd
sarath <91717756+sarathsekar@users.noreply.github.com>
2021-10-22 15:01:49 +0530

    csr,photographer space added in email subject

--- admin/create_photographer.php
+++ admin/create_photographer.php
@@ -61,7 +61,7 @@ function email($template,$fname,$email,$password,$secret_code,$con)
  $pcadmin_email=$get_profile['email'];
  $pcadmin_contact=$get_profile['contact_number'];
 
- $mail->Subject = "Your are created as an Photographer for".$get_profile['organization_name'];
+ $mail->Subject = "Your are created as an Photographer for ".$get_profile['organization_name'];
  $mail->Body = "<html><head><style>.titleCss {font-family: \"Roboto\",Helvetica,Arial,sans-serif;font-weight:600;font-size:18px;color:#0275D8 }.emailCss { width:100%;border:solid 1px #DDD;font-family: \"Roboto\",Helvetica,Arial,sans-serif; } </style></head><table cellpadding=\"5\" class=\"emailCss\"><tr><td align=\"left\"><img src=\"{{project_url}}logo.png\" /></td><td align=\"center\" class=\"titleCss\">You are created as photographer successfully!</td>
  <td align=\"right\"><img src=\""."{{project_url}}".$get_profile['logo_image_url']."\" width=\"110\" height=\"80\"/></td>  </tr><tr><td align=\"left\">info@fotopia.com<br>343 4543 213</td><td colspan=\"2\" align=\"right\">".strtoupper($get_profile['organization_name'])."<br>".$pcadmin_email."<br>".$pcadmin_contact."</td></tr><tr><td colspan=\"2\"><br><br>";
  //$mail->AltBody = "This is the plain text version of the email content";
