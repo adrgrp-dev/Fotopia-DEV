dedbd936673bd1ea73467a17fee22214099bdc95
Naveen Jones <91717751+NJ-21@users.noreply.github.com>
2021-11-15 12:10:13 +0530

    due date format changed

--- summary.php
+++ summary.php
@@ -380,7 +380,7 @@ $hs_id=$get_summary['home_seller_id'];
 				<td adr_trans="label_realtor_email">Realtors Email</td><td>:</td><td><?php echo $get_realtor_name["email"]; ?></td>
 				</tr>
 				<tr>
-				<td adr_trans="label_due_date">Due Date</td><td>:</td><td><?php echo $get_summary['order_due_date']; ?></td>
+				<td adr_trans="label_due_date">Due Date</td><td>:</td><td><?php echo date("d-m-Y",strtotime($get_summary['order_due_date'];)) ?></td>
 				</tr>
 				<tr>
 				<td adr_trans="label_booking_notes">Booking Notes</td><td>:</td><td><?php echo $get_summary['booking_notes']; ?></td>
